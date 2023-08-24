f8658ba19f81be2898c058e4d76a7da6d6c62254
Xiang Wang <ramwin@qq.com>
2023-08-24 21:51:39 +0800

    feat: 初始化仓库

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+  import {ref} from "vue"
+  const left_data = ref({
+    id: 18,
+    name: "alice",
+  })
+  const right_data = ref({
+    id: 18,
+    name: "bob",
+  })
+</script>
+
+<template>
+  <main>
+  </main>
+</template>
