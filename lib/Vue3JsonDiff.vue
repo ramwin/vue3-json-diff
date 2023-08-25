6f97f46659c505e3631037a3e855e4cd3eb5f525
Xiang Wang <ramwin@qq.com>
2023-08-25 23:41:31 +0800

    feat: 基础功能

--- lib/Vue3JsonDiff.vue
+++ lib/Vue3JsonDiff.vue
@@ -1,7 +1,52 @@
 <template>
-  <div>
-    我是vue组件
+  <div class="vue3-json-diff">
+    <div class="item">
+      <json-viewer :value="props.leftValue" />
+    </div>
+    <div class="item">
+      <div v-html="diffHtml"></div>
+    </div>
+    <div class="item">
+      <json-viewer :value="props.rightValue" />
+    </div>
   </div>
 </template>
 <script setup lang="ts">
+import { computed } from "vue"
+import { JsonViewer } from "vue3-json-viewer"
+import { diff_match_patch, diff_prettyHtml } from "diff-match-patch"
+
+const props = defineProps({
+  leftValue: {
+    type: null,
+    required: true,
+  },
+  rightValue: {
+    type: null,
+    required: true,
+  }
+})
+
+const diffHtml = computed(()=>{
+  const obj = new diff_match_patch()
+  const left_string = JSON.stringify(props.leftValue)
+  const right_string = JSON.stringify(props.rightValue)
+  console.log("计算区别", left_string, right_string)
+  const diff = obj.diff_main(
+    left_string, right_string
+  )
+  console.log("区别: ", diff)
+  const html = obj.diff_prettyHtml(
+    diff
+  )
+  return html
+})
 </script>
+<style scoped lang="less">
+.vue3-json-diff {
+  display: flex;
+  .item {
+    width: 30%;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,14 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, 'lib/main.js'),
+      entry: resolve(__dirname, 'lib/Vue3JsonDiff.vue'),
       name: 'Vue3JsonDiff',
       // the proper extensions will be added
       fileName: 'vue3-json-diff',
     },
     rollupOptions: {
       // 确保外部化处理那些你不想打包进库的依赖
-      external: ['vue'],
+      external: ['vue', "style.css"],
       output: {
         // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
         globals: {
