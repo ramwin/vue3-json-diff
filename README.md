0f290faa90d327fcc9a1be0cb8f53ab800432d92
Xiang Wang <ramwin@qq.com>
2023-08-26 22:26:05 +0800

    style: 添加了样式

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ npm install --save vue3-json-diff
 <script setup lang="ts">
   import {ref} from "vue"
   import Vue3JsonDiff from "vue3-json-diff"
+  import "vue3-json-diff/dist/style.css"
   const left_data = ref({
     id: 18,
     name: "alice",
--- lib/Vue3JsonDiff.vue
+++ lib/Vue3JsonDiff.vue
@@ -3,7 +3,7 @@
     <div class="item">
       <json-viewer :value="props.leftValue" />
     </div>
-    <div class="item">
+    <div class="item diff">
       <div v-html="diffHtml"></div>
     </div>
     <div class="item">
@@ -15,6 +15,7 @@
 import { computed } from "vue"
 import { JsonViewer } from "vue3-json-viewer"
 import { diff_match_patch, diff_prettyHtml } from "diff-match-patch"
+import "vue3-json-viewer/dist/index.css";
 
 const props = defineProps({
   leftValue: {
@@ -29,8 +30,8 @@ const props = defineProps({
 
 const diffHtml = computed(()=>{
   const obj = new diff_match_patch()
-  const left_string = JSON.stringify(props.leftValue)
-  const right_string = JSON.stringify(props.rightValue)
+  const left_string = JSON.stringify(props.leftValue, null, 4)
+  const right_string = JSON.stringify(props.rightValue, null, 4)
   console.log("计算区别", left_string, right_string)
   const diff = obj.diff_main(
     left_string, right_string
@@ -44,9 +45,19 @@ const diffHtml = computed(()=>{
 </script>
 <style scoped lang="less">
 .vue3-json-diff {
+  width: 100%;
+  padding: 0.3rem;
+  box-sizing: border-box;
   display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
   .item {
-    width: 30%;
+    width: 33%;
+    min-width: 300px;
+    border: 1px solid #ccc;
+    &.diff {
+      padding: 0.8rem;
+    }
   }
 }
 </style>
